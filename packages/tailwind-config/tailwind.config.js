2e682074c517eb9dd174ff2cbe97b52e59dac86e
Anton Bochkovskiy <unundertale1@ukr.net>
2025-01-04 11:39:25 +0200

    chore: update content paths and change container config

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['src/**/*.{js,ts,jsx,tsx}', 'app/**/*.{js,ts,jsx,tsx}', '../../packages/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    'src/**/*.{js,ts,jsx,tsx}',
+    'app/**/*.{js,ts,jsx,tsx}',
+    'components/**/*.{js,ts,jsx,tsx}',
+    '../../packages/ui/src/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
-    extend: {}
+    extend: {},
+    container: { center: true, padding: { DEFAULT: '1.25rem', md: '2rem' } }
   }
 };
