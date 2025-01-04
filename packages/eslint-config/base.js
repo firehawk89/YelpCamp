a25ae949e13c4fc6cfa085a5749044d800fd0b7f
Anton Bochkovskiy <unundertale1@ukr.net>
2025-01-04 11:38:44 +0200

    feat: add eslint prefectionist plugin

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,8 +1,9 @@
-import js from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
-import turboPlugin from "eslint-plugin-turbo";
-import tseslint from "typescript-eslint";
-import onlyWarn from "eslint-plugin-only-warn";
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import turboPlugin from 'eslint-plugin-turbo';
+import tseslint from 'typescript-eslint';
+import onlyWarn from 'eslint-plugin-only-warn';
+import perfectionist from 'eslint-plugin-perfectionist';
 
 /**
  * A shared ESLint configuration for the repository.
@@ -16,17 +17,25 @@ export const config = [
   {
     plugins: {
       turbo: turboPlugin,
+      perfectionist
     },
     rules: {
-      "turbo/no-undeclared-env-vars": "warn",
-    },
+      'turbo/no-undeclared-env-vars': 'warn',
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          type: 'natural',
+          order: 'asc'
+        }
+      ]
+    }
   },
   {
     plugins: {
-      onlyWarn,
-    },
+      onlyWarn
+    }
   },
   {
-    ignores: ["dist/**"],
-  },
+    ignores: ['dist/**', 'node_modules/**']
+  }
 ];
