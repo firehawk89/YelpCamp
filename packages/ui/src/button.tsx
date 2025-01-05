4a6ce100fa0d7912f0d020c9df68eac3d11bc777
Anton Bochkovskiy <unundertale1@ukr.net>
2025-01-05 19:16:08 +0200

    refactor: change basic button component configuration

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,20 +1,32 @@
-"use client";
+import { ButtonHTMLAttributes, FC } from 'react';
+import { VariantProps } from 'tailwind-variants';
+import { cn } from '@/utils/misc';
+import { tv } from 'tailwind-variants';
 
-import { ReactNode } from "react";
+export const buttonVariants = tv({
+  base: 'block rounded-md',
+  variants: {
+    variant: {
+      default: 'bg-orange-500 text-white hover:bg-opacity-80 transition-colors',
+      outline: 'bg-transparent border border-orange-500 hover:bg-orange-500 transition-colors'
+    },
+    size: {
+      default: 'px-3 py-2',
+      sm: 'px-2.5 py-1.5 text-sm'
+    }
+  },
+  defaultVariants: {
+    variant: 'default',
+    size: 'default'
+  }
+});
 
-interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
-}
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {}
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
-  return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
-      {children}
-    </button>
-  );
-};
+const Button: FC<ButtonProps> = ({ children, variant, size, className, ...props }) => (
+  <button className={cn(buttonVariants({ variant, size }), className)} {...props}>
+    {children}
+  </button>
+);
+
+export default Button;
