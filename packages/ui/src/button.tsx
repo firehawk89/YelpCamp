b2c648a288a8057dddc759cb64308bcf31812006
Anton Bochkovskiy <unundertale1@ukr.net>
2025-01-06 22:11:51 +0200

    feat: add new button variants

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -4,11 +4,15 @@ import { cn } from '@/utils/misc';
 import { tv } from 'tailwind-variants';
 
 export const buttonVariants = tv({
-  base: 'block rounded-md',
+  base: 'block rounded-md active:scale-95 transition-all',
   variants: {
     variant: {
-      default: 'bg-orange-500 text-white hover:bg-opacity-80 transition-colors',
-      outline: 'bg-transparent border border-orange-500 hover:bg-orange-500 transition-colors'
+      default: 'bg-neutral-500 text-white hover:bg-opacity-85',
+      outline: 'bg-transparent border border-orange-500 text-orange-500 hover:bg-orange-500 hover:text-white',
+      accent: 'bg-orange-500 text-white hover:bg-opacity-85',
+      info: 'bg-blue-500 text-white hover:bg-opacity-85',
+      success: 'bg-green-600 text-white hover:bg-opacity-85',
+      error: 'bg-red-600 text-white hover:bg-opacity-85'
     },
     size: {
       default: 'px-3 py-2',
