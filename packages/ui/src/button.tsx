0808bde2752eb93a052092aee259818071b7966e
Anton Bochkovskiy <unundertale1@ukr.net>
2025-01-12 17:11:13 +0200

    feat: add compound variants for button

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -4,11 +4,11 @@ import { cn } from '@/utils/misc';
 import { tv } from 'tailwind-variants';
 
 export const buttonVariants = tv({
-  base: 'block rounded-md active:scale-95 transition-all',
+  base: 'flex items-center rounded-lg active:scale-95 transition-all',
   variants: {
     variant: {
       default: 'bg-neutral-500 text-white hover:bg-opacity-85',
-      outline: 'bg-transparent border border-orange-500 text-orange-500 hover:bg-orange-500 hover:text-white',
+      outline: 'bg-transparent border border-neutral-500 text-neutral-500 hover:bg-neutral-500 hover:text-white',
       accent: 'bg-orange-500 text-white hover:bg-opacity-85',
       info: 'bg-blue-500 text-white hover:bg-opacity-85',
       success: 'bg-green-600 text-white hover:bg-opacity-85',
@@ -16,19 +16,51 @@ export const buttonVariants = tv({
     },
     size: {
       default: 'px-3 py-2',
-      sm: 'px-2.5 py-1.5 text-sm'
+      sm: 'px-2 py-1 text-sm'
+    },
+    color: {
+      default: 'text-white',
+      accent: 'text-orange-500',
+      info: 'text-blue-500',
+      success: 'text-green-600',
+      error: 'text-red-600'
     }
   },
   defaultVariants: {
     variant: 'default',
-    size: 'default'
-  }
+    size: 'default',
+    color: 'default'
+  },
+  compoundVariants: [
+    {
+      variant: 'outline',
+      color: 'accent',
+      className: 'border-orange-500 hover:bg-orange-500 hover:text-white'
+    },
+    {
+      variant: 'outline',
+      color: 'info',
+      className: 'border-blue-500 hover:bg-blue-500 hover:text-white'
+    },
+    {
+      variant: 'outline',
+      color: 'success',
+      className: 'border-green-600 hover:bg-green-600 hover:text-white'
+    },
+    {
+      variant: 'outline',
+      color: 'error',
+      className: 'border-red-600 hover:bg-red-600 hover:text-white'
+    }
+  ]
 });
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {}
+interface ButtonProps
+  extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'color'>,
+    VariantProps<typeof buttonVariants> {}
 
-const Button: FC<ButtonProps> = ({ children, variant, size, className, ...props }) => (
-  <button className={cn(buttonVariants({ variant, size }), className)} {...props}>
+const Button: FC<ButtonProps> = ({ children, variant, size, color, className, ...props }) => (
+  <button className={cn(buttonVariants({ variant, size, color }), className)} {...props}>
     {children}
   </button>
 );
