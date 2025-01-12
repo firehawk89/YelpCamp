d696e01a8de78786196879e1771086cb99d433e9
Anton Bochkovskiy <unundertale1@ukr.net>
2025-01-12 17:12:14 +0200

    feat: add header navigation link component

---
+++ apps/web/components/layout/Header/HeaderLink.tsx
@@ -0,0 +1,16 @@
+import { cn } from '@/utils/misc';
+import Link, { LinkProps } from 'next/link';
+import { FC, PropsWithChildren, ReactNode } from 'react';
+
+interface HeaderAuthLinkProps extends PropsWithChildren, LinkProps {
+  className?: string;
+  icon?: ReactNode;
+}
+
+const HeaderLink: FC<HeaderAuthLinkProps> = ({ icon, children, className, ...props }) => (
+  <Link className={cn('flex items-center gap-1 hover:text-orange-500 transition-colors', className)} {...props}>
+    {icon} {children}
+  </Link>
+);
+
+export default HeaderLink;
