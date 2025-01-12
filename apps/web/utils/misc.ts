687b86164cd8c3bf23633554baa554f2ad7c87ef
Anton Bochkovskiy <unundertale1@ukr.net>
2025-01-12 17:11:54 +0200

    feat: add isHomePage utility

--- apps/web/utils/misc.ts
+++ apps/web/utils/misc.ts
@@ -2,3 +2,5 @@ import { ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+
+export const isHomePage = (pathname: string) => pathname === '/';
