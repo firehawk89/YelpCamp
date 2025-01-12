c09c72f17cb966b503802ea58b4a6aaa7acd95f5
Anton Bochkovskiy <unundertale1@ukr.net>
2025-01-12 17:12:36 +0200

    feat: add logo component

--- apps/web/components/shared/Logo.tsx
+++ apps/web/components/shared/Logo.tsx
@@ -1,11 +1,26 @@
-import Link from 'next/link';
-import { AnchorHTMLAttributes, FC } from 'react';
-import { cn } from 'utils/misc';
+'use client';
 
-const Logo: FC<AnchorHTMLAttributes<HTMLAnchorElement>> = ({ className, ...props }) => (
-  <Link className={cn('text-2xl font-extrabold text-white', className)} href="/" {...props}>
-    YelpCamp
-  </Link>
-);
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { AnchorHTMLAttributes, FC } from 'react';
+import { cn, isHomePage } from 'utils/misc';
+
+const Logo: FC<AnchorHTMLAttributes<HTMLAnchorElement>> = ({ className, ...props }) => {
+  const pathname = usePathname();
+
+  return (
+    <Link
+      className={cn(
+        'text-2xl font-extrabold hover:text-orange-500 transition-colors',
+        isHomePage(pathname) && 'text-white',
+        className
+      )}
+      href="/"
+      {...props}
+    >
+      YelpCamp
+    </Link>
+  );
+};
 
 export default Logo;
