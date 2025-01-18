4c771b96f62bf44e8c2f840d10737e9b6b4c12d6
Anton Bochkovskiy <unundertale1@ukr.net>
2025-01-18 18:38:25 +0200

    feat: add create review DTO

---
+++ apps/api/src/dto/review/create-review.dto.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, IsNumber, IsOptional, IsString, Max, Min } from 'class-validator';
+
+export class CreateReviewDTO {
+  @IsOptional()
+  @IsString({ message: 'Review text must be a string' })
+  body: string;
+
+  @IsNotEmpty({ message: 'Rating is required' })
+  @Min(1, { message: 'Rating must be at least 1' })
+  @Max(5, { message: 'Rating must be at most 5' })
+  @IsNumber(undefined, { message: 'Rating must be a number' })
+  rating: number;
+}
