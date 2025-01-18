07d1b855eeb2e6060f4fa43cbf9f6db042dc95eb
Anton Bochkovskiy <unundertale1@ukr.net>
2025-01-18 18:32:42 +0200

    feat: use config and mongoose modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CampgroundsModule } from './campgrounds/campgrounds.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ReviewsModule } from './reviews/reviews.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DATABASE_URL), CampgroundsModule, ReviewsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
