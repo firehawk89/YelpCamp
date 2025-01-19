bd3db75ccfd2358d52d7d29e9ab33f98313ead1a
Anton Bochkovskiy <unundertale1@ukr.net>
2025-01-19 20:54:43 +0200

    feat: add campground model usage support in reviews module

--- apps/api/src/reviews/reviews.module.ts
+++ apps/api/src/reviews/reviews.module.ts
@@ -3,9 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Review, ReviewSchema } from 'src/schemas/review.schema';
 import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
+import { Campground, CampgroundSchema } from 'src/schemas/campground.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Review.name, schema: ReviewSchema }])],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Review.name, schema: ReviewSchema },
+      { name: Campground.name, schema: CampgroundSchema }
+    ])
+  ],
   providers: [ReviewsService],
   controllers: [ReviewsController]
 })
