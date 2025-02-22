d125df3264c1f2e7d6c3d1188912989be13bac4e
Anton Bochkovskiy <unundertale1@ukr.net>
2025-02-22 16:51:25 +0200

    fix: move jwt module import from auth module to app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import config from './config';
+import { ACCESS_TOKEN_EXPIRATION_TIME } from './helpers/constants';
 import { AuthModule } from './modules/auth/auth.module';
 import { CampgroundsModule } from './modules/campgrounds/campgrounds.module';
 import { ReviewsModule } from './modules/reviews/reviews.module';
@@ -17,6 +19,15 @@ import { UsersModule } from './modules/users/users.module';
       imports: [ConfigModule],
       useFactory: async (config: ConfigService) => ({ uri: config.get<string>('database.url') }),
       inject: [ConfigService],
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('jwt.secret'),
+        signOptions: { expiresIn: ACCESS_TOKEN_EXPIRATION_TIME },
+      }),
+      inject: [ConfigService],
+      global: true,
     }),
     AuthModule,
     UsersModule,
--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ACCESS_TOKEN_EXPIRATION_TIME } from 'src/helpers/constants';
 import { RefreshToken, RefreshTokenSchema } from 'src/schemas/refresh-token.schema';
 import { User, UserSchema } from 'src/schemas/user.schema';
 
@@ -15,14 +12,6 @@ import { AuthService } from './auth.service';
       { name: User.name, schema: UserSchema },
       { name: RefreshToken.name, schema: RefreshTokenSchema },
     ]),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: (config: ConfigService) => ({
-        secret: config.get<string>('jwt.secret'),
-        signOptions: { expiresIn: ACCESS_TOKEN_EXPIRATION_TIME },
-      }),
-      inject: [ConfigService],
-    }),
   ],
   providers: [AuthService],
   controllers: [AuthController],
