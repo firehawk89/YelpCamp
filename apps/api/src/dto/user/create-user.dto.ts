c65e52ce36cfd5277961981289ef8f2ad00ed326
Anton Bochkovskiy <unundertale1@ukr.net>
2025-03-15 21:17:19 +0200

    feat: add create user request data dto

---
+++ apps/api/src/dto/user/create-user.dto.ts
@@ -0,0 +1,17 @@
+import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
+
+export class CreateUserDTO {
+  @IsNotEmpty({ message: 'Email is required' })
+  @IsEmail({}, { message: 'Email address should be valid' })
+  email: string;
+
+  @IsNotEmpty({ message: 'Password is required' })
+  @IsStrongPassword(
+    { minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1 },
+    {
+      message:
+        'Password must be at least 8 characters long and contain at least 1 lowercase letter, 1 uppercase letter, and 1 number',
+    }
+  )
+  password: string;
+}
