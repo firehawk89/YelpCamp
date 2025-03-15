a8cbd50e5b50c2f45b47fc89aeef725305540764
Anton Bochkovskiy <unundertale1@ukr.net>
2025-03-15 22:01:40 +0200

    feat: implement debounce utility function

--- apps/web/types/api.ts
+++ apps/web/types/api.ts
@@ -1,6 +1,6 @@
 export type ApiResponse<T> = Promise<{
   result?: T;
-  error?: string;
+  error?: string | string[];
 }>;
 
 export interface PaginationMetadata {
--- apps/web/utils/misc.ts
+++ apps/web/utils/misc.ts
@@ -16,3 +16,14 @@ export const getSearchParamsString = (params: Record<string, unknown>, existingP
 
   return searchParams.toString();
 };
+
+export const debounce = <F extends (...args: Parameters<F>) => ReturnType<F>>(func: F, waitFor: number) => {
+  let timeout: NodeJS.Timeout;
+
+  const debounced = (...args: Parameters<F>) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), waitFor);
+  };
+
+  return debounced;
+};
