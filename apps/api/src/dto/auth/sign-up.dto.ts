fbaf7a568b3e0ac0ac33440891ccdbc66d2653da
Anton Bochkovskiy <unundertale1@ukr.net>
2025-03-15 21:40:49 +0200

    fix: adjust password validation in the sign up dto

--- apps/api/src/dto/auth/sign-up.dto.ts
+++ apps/api/src/dto/auth/sign-up.dto.ts
@@ -7,10 +7,10 @@ export class SignUpDTO {
 
   @IsNotEmpty({ message: 'Password is required' })
   @IsStrongPassword(
-    { minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1 },
+    { minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1 },
     {
       message:
-        'Password must be at least 8 characters long and contain at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 symbol',
+        'Password must be at least 8 characters long and contain at least 1 lowercase letter, 1 uppercase letter, and 1 number',
     }
   )
   password: string;
