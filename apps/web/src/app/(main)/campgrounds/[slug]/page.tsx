0bee16e44bef890b68a63a8a024e4241ddef6937
Anton Bochkovskiy <unundertale1@ukr.net>
2025-04-03 22:46:57 +0300

    feat: add campground details page

---
+++ apps/web/src/app/(main)/campgrounds/[slug]/page.tsx
@@ -0,0 +1,40 @@
+import CampgroundLocation from '@/modules/campgrounds/components/CampgroundLocation';
+import CampgroundRating from '@/modules/campgrounds/components/CampgroundRating';
+import Reviews from '@/modules/campgrounds/components/Reviews';
+import ReviewsChip from '@/modules/campgrounds/components/ReviewsChip';
+import { fetchCampground } from '@/utils/api/campgrounds';
+import ImagePlaceholder from '@repo/ui/image-placeholder';
+
+interface CampgroundProps {
+  params: Promise<{ slug: string }>;
+}
+
+export default async function Campground({ params }: CampgroundProps) {
+  const { slug } = await params;
+  const { result: campground, error } = await fetchCampground(slug);
+
+  if (error) {
+    throw new Error(typeof error === 'string' ? error : error.join(', '));
+  }
+
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="flex flex-col items-center gap-5">
+        <h1 className="text-center text-3xl font-bold lg:text-4xl">{campground?.title}</h1>
+
+        <div className="flex items-center justify-center gap-3">
+          <CampgroundRating rating={campground?.rating} />
+          <ReviewsChip reviewsCount={campground?.reviewsCount} href="#reviews" />
+          {campground?.location && <CampgroundLocation location={campground?.location} />}
+        </div>
+
+        <div className="flex flex-col gap-3 text-center lg:w-[75%]">
+          <ImagePlaceholder className="mx-auto aspect-video w-full shrink-0 rounded-lg" />
+          {campground?.description && <p className="text-neutral-700">{campground?.description}</p>}
+        </div>
+
+        {campground && <Reviews campground={campground} />}
+      </div>
+    </div>
+  );
+}
--- apps/web/src/utils/api/campgrounds.ts
+++ apps/web/src/utils/api/campgrounds.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { PaginatedApiResponse, PaginatedResponse } from '@/types/api';
+import { ApiResponse, PaginatedApiResponse, PaginatedResponse } from '@/types/api';
 import { Campground, CampgroundsFilterDto } from '@/types/campground';
 
 import { API_ROUTES } from '../constants';
@@ -16,9 +16,26 @@ export const fetchCampgrounds = async (props: CampgroundsFilterDto): PaginatedAp
     }
 
     const result: PaginatedResponse<Campground> = await response.json();
+
     return { result };
   } catch (err) {
     const errorMessage = err instanceof Error ? err.message : 'Failed to fetch campgrounds';
+    return { error: errorMessage };
+  }
+};
+
+export const fetchCampground = async (slug: string): ApiResponse<Campground> => {
+  try {
+    const response = await fetch(`${API_ROUTES.CAMPGROUNDS}/${slug}`);
+    if (!response.ok) {
+      return { error: 'Failed to fetch a campground' };
+    }
+
+    const result: Campground = await response.json();
+
+    return { result };
+  } catch (err) {
+    const errorMessage = err instanceof Error ? err.message : 'Failed to fetch a campground';
     return { error: errorMessage };
   }
 };
