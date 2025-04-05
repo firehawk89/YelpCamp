c1386a3d5259fe70fd1ca7daa175a474f8adf678
Anton Bochkovskiy <unundertale1@ukr.net>
2025-04-05 19:56:43 +0300

    fix: update create review dto

--- apps/api/src/dto/review/create-review.dto.ts
+++ apps/api/src/dto/review/create-review.dto.ts
@@ -1,9 +1,31 @@
-import { IsNotEmpty, IsNumber, IsOptional, IsString, Max, Min } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsOptional, IsString, Max, MaxLength, Min, MinLength } from 'class-validator';
+import {
+  MAX_REVIEW_BODY_LENGTH,
+  MAX_REVIEW_TITLE_LENGTH,
+  MIN_REVIEW_BODY_LENGTH,
+  MIN_REVIEW_TITLE_LENGTH,
+} from 'src/helpers/constants';
 
 export class CreateReviewDTO {
   @IsOptional()
+  @IsString({ message: 'Review title must be a string' })
+  @MinLength(MIN_REVIEW_TITLE_LENGTH, {
+    message: `Review title must be at least ${MIN_REVIEW_TITLE_LENGTH} characters long`,
+  })
+  @MaxLength(MAX_REVIEW_TITLE_LENGTH, {
+    message: `Review title must be at least ${MAX_REVIEW_TITLE_LENGTH} characters long`,
+  })
+  title?: string;
+
+  @IsNotEmpty({ message: 'Review text is required' })
   @IsString({ message: 'Review text must be a string' })
-  body?: string;
+  @MinLength(MIN_REVIEW_BODY_LENGTH, {
+    message: `Review text must be at least ${MIN_REVIEW_BODY_LENGTH} characters long`,
+  })
+  @MaxLength(MAX_REVIEW_BODY_LENGTH, {
+    message: `Review text must be at least ${MAX_REVIEW_BODY_LENGTH} characters long`,
+  })
+  body: string;
 
   @IsNotEmpty({ message: 'Rating is required' })
   @Min(1, { message: 'Rating must be at least 1' })
--- apps/api/src/helpers/constants.ts
+++ apps/api/src/helpers/constants.ts
@@ -5,6 +5,10 @@ export const DEFAULT_PAGE_LIMIT: number = 4;
 
 export const MIN_RATING: number = 1;
 export const MAX_RATING: number = 5;
+export const MIN_REVIEW_BODY_LENGTH: number = 10;
+export const MAX_REVIEW_BODY_LENGTH: number = 1800;
+export const MIN_REVIEW_TITLE_LENGTH: number = 2;
+export const MAX_REVIEW_TITLE_LENGTH: number = 100;
 
 export const DEFAULT_SORT_FIELD = 'createdAt';
 export const DEFAULT_SORT_ORDER: SortOrder = 'desc';
