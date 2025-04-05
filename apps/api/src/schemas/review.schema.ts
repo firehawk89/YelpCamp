c24ec387ed6e58bc67c9c0e621fdf9862a4193a1
Anton Bochkovskiy <unundertale1@ukr.net>
2025-04-05 19:56:04 +0300

    fix: include review title and likedBy fileds in the review schema

--- apps/api/src/schemas/review.schema.ts
+++ apps/api/src/schemas/review.schema.ts
@@ -1,21 +1,34 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
-import { MIN_RATING, MAX_RATING } from 'src/helpers/constants';
+import {
+  MAX_RATING,
+  MAX_REVIEW_BODY_LENGTH,
+  MAX_REVIEW_TITLE_LENGTH,
+  MIN_RATING,
+  MIN_REVIEW_BODY_LENGTH,
+  MIN_REVIEW_TITLE_LENGTH,
+} from 'src/helpers/constants';
 
 export type ReviewDocument = HydratedDocument<Review>;
 
 @Schema({ timestamps: true })
 export class Review {
-  @Prop({ required: false, default: null })
-  body?: string;
-
   @Prop({ required: true, min: MIN_RATING, max: MAX_RATING })
   rating: number;
+
+  @Prop({ required: false, default: null, minlength: MIN_REVIEW_TITLE_LENGTH, maxlength: MAX_REVIEW_TITLE_LENGTH })
+  title?: string;
+
+  @Prop({ required: true, minlength: MIN_REVIEW_BODY_LENGTH, maxlength: MAX_REVIEW_BODY_LENGTH })
+  body: string;
+
+  @Prop({ type: [mongoose.Schema.Types.ObjectId], ref: 'User', required: false })
+  likedBy?: mongoose.Types.ObjectId[];
 
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Campground', required: true })
   campgroundId: mongoose.Types.ObjectId;
 
-  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
   author: mongoose.Types.ObjectId;
 }
 
