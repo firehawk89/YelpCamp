9e2cb5a311b6243ef6144ffb6cd428a7c3c0d443
Anton Bochkovskiy <unundertale1@ukr.net>
2025-04-26 21:38:58 +0300

    feat: add PaginatedContent component and usePaginatedData hook

---
+++ apps/web/src/components/PaginatedContent.tsx
@@ -0,0 +1,68 @@
+import LoadMoreButton from '@/modules/campgrounds/components/Reviews/LoadMoreButton';
+import { cn } from '@/utils/misc';
+import Alert from '@repo/ui/alert';
+import { HTMLAttributes } from 'react';
+
+import Pagination from './Pagination';
+
+interface PaginatedContentProps extends HTMLAttributes<HTMLDivElement> {
+  elementsCount: number;
+  error: string | null;
+  isLoading: boolean;
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => Promise<void>;
+  pagination?: boolean;
+  emptyStateMessage?: string;
+}
+
+const PaginatedContent = ({
+  elementsCount,
+  error,
+  isLoading,
+  currentPage,
+  totalPages,
+  onPageChange,
+  emptyStateMessage = 'No data found',
+  children,
+  className,
+  pagination = true,
+  ...props
+}: PaginatedContentProps) => {
+  const hasData = elementsCount > 0;
+  const hasMore = currentPage < totalPages;
+  const showEmptyState = !error && !hasData;
+  const showContent = !error && hasData;
+
+  return (
+    <div className={cn('flex w-full min-w-80 flex-col gap-6', className)} {...props}>
+      {showEmptyState && (
+        <Alert className="w-full" color="info">
+          {emptyStateMessage}
+        </Alert>
+      )}
+
+      {error && (
+        <Alert className="w-full" color="danger">
+          {error}
+        </Alert>
+      )}
+
+      {showContent && children}
+
+      {hasMore &&
+        (pagination ? (
+          <Pagination className="mt-5" page={currentPage} totalPages={totalPages} />
+        ) : (
+          <LoadMoreButton
+            page={currentPage}
+            totalPages={totalPages}
+            onPageChange={onPageChange}
+            isLoading={isLoading}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default PaginatedContent;
---
+++ apps/web/src/hooks/usePaginatedData.ts
@@ -0,0 +1,85 @@
+import { PaginatedApiResponse, PaginatedResponse } from '@/types/api';
+import { DEFAULT_PAGE } from '@/utils/constants/defaults';
+import { useCallback, useEffect, useState } from 'react';
+
+interface UsePaginatedDataProps<T> {
+  initialData?: PaginatedResponse<T>;
+  fetchPageData: (newPage: number) => PaginatedApiResponse<T>;
+  defaultErrorMessage?: string;
+}
+
+interface UsePaginatedDataReturn<T> {
+  data: T[];
+  currentPage: number;
+  isLoading: boolean;
+  error: string | null;
+  hasMore: boolean;
+  fetchNextPage: (newPage: number) => Promise<void>;
+}
+
+export function usePaginatedData<T extends { _id: string }>({
+  initialData,
+  fetchPageData,
+  defaultErrorMessage = 'Failed to load more data',
+}: UsePaginatedDataProps<T>): UsePaginatedDataReturn<T> {
+  const [data, setData] = useState<T[]>(initialData?.data ?? []);
+  const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const { totalPages = 0 } = initialData?.metadata ?? {};
+  const hasMore = currentPage < totalPages;
+
+  const fetchNextPage = useCallback(
+    async (newPage: number) => {
+      if (newPage <= currentPage || isLoading || newPage > totalPages) {
+        return;
+      }
+
+      setIsLoading(true);
+      setError(null);
+
+      try {
+        const { result, error: fetchError } = await fetchPageData(newPage);
+
+        if (fetchError) {
+          throw new Error(typeof fetchError === 'string' ? fetchError : fetchError.join(', '));
+        }
+
+        if (result?.data?.length) {
+          setCurrentPage(newPage);
+
+          setData((prevData) => {
+            const uniqueNewData =
+              result?.data.filter((newItem) => !prevData.some((prevItem) => prevItem._id === newItem._id)) || [];
+
+            return [...prevData, ...uniqueNewData];
+          });
+        } else if (result?.data && result.data.length === 0) {
+          setCurrentPage(totalPages);
+        }
+      } catch (error) {
+        setError(error instanceof Error ? error.message : defaultErrorMessage);
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [currentPage, defaultErrorMessage, fetchPageData, isLoading, totalPages]
+  );
+
+  useEffect(() => {
+    if (initialData?.data?.length) {
+      setData(initialData.data);
+      setCurrentPage(initialData.metadata?.page || DEFAULT_PAGE);
+    }
+  }, [initialData]);
+
+  return {
+    data,
+    currentPage,
+    isLoading,
+    error,
+    hasMore,
+    fetchNextPage,
+  };
+}
