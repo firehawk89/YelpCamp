eb27c6075dd4f57a7dae39aa668454e3c03b4f14
Anton Bochkovskiy <unundertale1@ukr.net>
2025-04-26 21:41:14 +0300

    refactor: update Reviews component structure

--- apps/web/src/modules/campgrounds/components/Reviews/index.tsx
+++ apps/web/src/modules/campgrounds/components/Reviews/index.tsx
@@ -1,17 +1,15 @@
 'use client';
 
+import PaginatedContent from '@/components/PaginatedContent';
+import { usePaginatedData } from '@/hooks/usePaginatedData';
 import { fetchCampgroundReviews } from '@/server/reviews';
 import { fetchUserReviews } from '@/server/user';
 import { PaginatedResponse } from '@/types/api';
 import { Campground } from '@/types/campground';
 import { Review } from '@/types/review';
 import { User } from '@/types/user';
-import { DEFAULT_PAGE } from '@/utils/constants/defaults';
-import { cn } from '@/utils/misc';
-import Alert from '@repo/ui/alert';
-import { HTMLAttributes, useCallback, useEffect, useState } from 'react';
+import { HTMLAttributes, useCallback } from 'react';
 
-import LoadMoreButton from './LoadMoreButton';
 import ReviewsHeader from './ReviewsHeader';
 import ReviewsList from './ReviewsList';
 
@@ -23,106 +21,61 @@ interface ReviewsProps extends HTMLAttributes<HTMLDivElement> {
   mode?: 'campground' | 'user';
 }
 
-const Reviews = ({ user, reviewsData, campground, error, mode = 'campground', className, ...props }: ReviewsProps) => {
-  const [reviews, setReviews] = useState<Review[]>(reviewsData?.data ?? []);
-  const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE);
-  const [isLoading, setIsLoading] = useState(false);
-  const [loadError, setLoadError] = useState<string | null>(null);
+const Reviews = ({
+  user,
+  reviewsData,
+  campground,
+  error: initialError,
+  mode = 'campground',
+  ...props
+}: ReviewsProps) => {
+  const fetchNewPage = useCallback(
+    async (newPage: number) => {
+      const fetchReviews = mode === 'campground' ? fetchCampgroundReviews : fetchUserReviews;
+      const targetId = mode === 'campground' ? campground?._id : user?._id;
+
+      if (!targetId) {
+        return {
+          error: `Cannot fetch reviews: missing ${mode} ID`,
+        };
+      }
+
+      return fetchReviews(targetId, { page: newPage.toString() });
+    },
+    [campground?._id, mode, user?._id]
+  );
+
+  const {
+    data: reviews,
+    currentPage,
+    isLoading,
+    error,
+    fetchNextPage,
+  } = usePaginatedData<Review>({
+    initialData: reviewsData,
+    fetchPageData: fetchNewPage,
+    defaultErrorMessage: 'Failed to load more reviews',
+  });
 
   const { totalPages = 0 } = reviewsData?.metadata ?? {};
 
-  const hasReviews = reviews.length > 0;
-  const hasMoreReviews = currentPage < totalPages;
-  const showEmptyState = !error && !loadError && !hasReviews;
-  const showReviewsList = !error && !loadError && hasReviews;
-
-  const fetchReviewsByPage = useCallback(
-    async (newPage: number) => {
-      if (newPage <= currentPage || isLoading || newPage > totalPages) {
-        return;
-      }
-
-      setIsLoading(true);
-      setLoadError(null);
-
-      try {
-        const fetchReviews = mode === 'campground' ? fetchCampgroundReviews : fetchUserReviews;
-        const targetId = mode === 'campground' ? campground?._id : user?._id;
-
-        if (!targetId) {
-          throw new Error(`Cannot fetch reviews: missing ${mode} ID`);
-        }
-
-        const { result, error } = await fetchReviews(targetId, { page: newPage.toString() });
-
-        if (error) {
-          throw new Error(typeof error === 'string' ? error : error.join(', '));
-        }
-
-        if (result?.data?.length) {
-          setCurrentPage(newPage);
-
-          setReviews((prevReviews) => {
-            const uniqueNewReviews =
-              result?.data.filter((newReview) => !prevReviews.some((prevReview) => prevReview._id === newReview._id)) ||
-              [];
-
-            return [...prevReviews, ...uniqueNewReviews];
-          });
-        } else if (result?.data && result.data.length === 0) {
-          setCurrentPage(totalPages);
-        }
-      } catch (error) {
-        setLoadError(error instanceof Error ? error.message : 'Failed to load more reviews');
-      } finally {
-        setIsLoading(false);
-      }
-    },
-    [campground?._id, currentPage, isLoading, mode, totalPages, user?._id]
-  );
-
-  useEffect(() => {
-    if (reviewsData?.data?.length) {
-      setReviews(reviewsData.data);
-      setCurrentPage(reviewsData.metadata?.page || DEFAULT_PAGE);
-    }
-  }, [reviewsData]);
-
   return (
-    <div id="reviews" className={cn('flex w-full min-w-80 flex-col gap-6', className)} {...props}>
+    <PaginatedContent
+      elementsCount={reviews.length}
+      error={initialError || error}
+      isLoading={isLoading}
+      currentPage={currentPage}
+      totalPages={totalPages}
+      onPageChange={fetchNextPage}
+      emptyStateMessage="No reviews yet"
+      pagination={false}
+      {...props}
+    >
       {mode === 'campground' && campground && (
         <ReviewsHeader userId={user?._id} campground={campground} reviews={reviews} />
       )}
-
-      {showEmptyState && (
-        <Alert className="w-full" color="info">
-          No reviews yet
-        </Alert>
-      )}
-
-      {error && (
-        <Alert className="w-full" color="danger">
-          {error}
-        </Alert>
-      )}
-
-      {loadError && (
-        <Alert className="w-full" color="danger">
-          {loadError}
-        </Alert>
-      )}
-
-      {showReviewsList && <ReviewsList reviews={reviews} user={user} mode={mode} />}
-
-      {hasMoreReviews && (
-        <LoadMoreButton
-          page={currentPage}
-          totalPages={totalPages}
-          onPageChange={fetchReviewsByPage}
-          isLoading={isLoading}
-        />
-      )}
-    </div>
+      <ReviewsList reviews={reviews} user={user} mode={mode} />
+    </PaginatedContent>
   );
 };
 
