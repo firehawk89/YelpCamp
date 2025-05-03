e9369d02031ff7094533dee79c913e97d645909d
Anton Bochkovskiy <unundertale1@ukr.net>
2025-05-03 12:54:34 +0300

    refactor: update GeocoderInput component

--- apps/web/src/components/GeocoderInput.tsx
+++ apps/web/src/components/GeocoderInput.tsx
@@ -1,34 +1,47 @@
 'use client';
 
+import type { GeocodeFeature } from '@mapbox/search-js-core';
+
 import config from '@/app/config';
 import { Geocoder } from '@mapbox/search-js-react';
 import { GeocoderProps } from '@mapbox/search-js-react/dist/components/Geocoder';
-import { useState } from 'react';
 
-interface GeocoderInputProps extends Omit<GeocoderProps, 'accessToken'> {
-  className?: string;
+export interface GeocodingData {
+  full_address: GeocodeFeature['properties']['full_address'];
+  coordinates: GeocodeFeature['properties']['coordinates'];
 }
 
-const GeocoderInput = ({ className, ...props }: GeocoderInputProps) => {
-  const [inputValue, setInputValue] = useState('');
-  console.log('inputValue', { inputValue });
+interface GeocoderInputProps extends Omit<GeocoderProps, 'accessToken' | 'value' | 'onChange' | 'onRetrieve'> {
+  value: string;
+  onChange: (value: string) => void;
+  onRetrieve: (res: GeocodingData) => void;
+  onClear?: () => void;
+  containerClassName?: string;
+}
+
+const GeocoderInput = ({ value, onChange, onRetrieve, onClear, containerClassName, ...props }: GeocoderInputProps) => {
+  const handleSelect = (feature: GeocodeFeature) => {
+    const { full_address, coordinates } = feature.properties;
+
+    const geocodingData: GeocodingData = {
+      full_address: full_address,
+      coordinates: {
+        latitude: coordinates.latitude,
+        longitude: coordinates.longitude,
+      },
+    };
+
+    onRetrieve(geocodingData);
+  };
 
   return (
-    <div className={className}>
-      {/* <SearchBox
-        accessToken={config.mapbox.accessToken}
-        value={inputValue}
-        onRetrieve={(res) => console.log('onRetrieve', res)}
-        onChange={setInputValue}
-      >
-        <input placeholder="address" />
-      </SearchBox> */}
-
+    <div className={containerClassName}>
       {/* @ts-expect-error: The Mapbox Geocoder component is not recognized as a valid JSX component due to missing type definitions in the library. */}
       <Geocoder
-        onChange={setInputValue}
-        onRetrieve={(res) => console.log('onRetrieve', res)}
-        onClear={() => setInputValue('')}
+        value={value}
+        onChange={onChange}
+        onRetrieve={handleSelect}
+        onClear={onClear}
         accessToken={config.mapbox.accessToken}
         {...props}
       />
