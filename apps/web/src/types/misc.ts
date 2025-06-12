44daedffecb099034b635334e8bb2258b86c25ab
Anton Bochkovskiy <unundertale1@ukr.net>
2025-06-12 20:22:09 +0300

    feat: add next-intl types augmentation

--- apps/web/src/types/misc.ts
+++ apps/web/src/types/misc.ts
@@ -1,1 +1,4 @@
+import type { createTranslator, Messages } from 'next-intl';
+
 export type SearchParams = { [key: string]: string | string[] | undefined };
+export type TFunction = ReturnType<typeof createTranslator<Messages>>;
