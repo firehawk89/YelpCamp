469fb05f156bbf13d487001fe4b5810ac2982365
Anton Bochkovskiy <unundertale1@ukr.net>
2025-11-08 13:57:46 +0100

    refactor: remove LogOutDTO and update validation messages in SignInDTO and SignUpDTO to improve clarity and consistency

--- apps/api/src/dto/auth/log-out.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsMongoId, IsNotEmpty } from 'class-validator';
-
-export class LogOutDTO {
-  @IsNotEmpty({ message: 'User ID is required' })
-  @IsMongoId({ message: 'Invalid user ID format' })
-  userId: string;
-}
--- apps/api/src/dto/auth/sign-in.dto.ts
+++ apps/api/src/dto/auth/sign-in.dto.ts
@@ -1,16 +1,16 @@
+import { MIN_PASSWORD_LENGTH } from '@repo/constants';
 import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
 
 export class SignInDTO {
   @IsNotEmpty({ message: 'Email is required' })
-  @IsEmail({}, { message: 'Email address should be valid' })
+  @IsEmail({}, { message: 'Email address is invalid' })
   email: string;
 
   @IsNotEmpty({ message: 'Password is required' })
   @IsStrongPassword(
-    { minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1 },
+    { minLength: MIN_PASSWORD_LENGTH, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1 },
     {
-      message:
-        'Password must be at least 8 characters long and contain at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 symbol',
+      message: `Password must contain at least ${MIN_PASSWORD_LENGTH} characters long and at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 symbol`,
     }
   )
   password: string;
