4a0e33c40436212f6e7d96cf29a57d03788f9208
Anton Bochkovskiy <unundertale1@ukr.net>
2025-12-07 16:19:19 +0100

    feat(ui): integrate Storybook for component development and testing

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,4 +1,5 @@
-import { config } from "@repo/eslint-config/react-internal";
+import { config } from '@repo/eslint-config/react-internal';
+import storybook from 'eslint-plugin-storybook';
 
 /** @type {import("eslint").Linter.Config} */
-export default config;
+export default [...config, ...storybook.configs['flat/recommended']];
